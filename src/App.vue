fa2cff263e05ed8c20a44ec4c5321e22c75620ef
pierre pracht <pierre.pracht@gmail.com>
2023-05-07 14:59:57 +0200

    toutes les offres

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
-import MaisonCard from './components/MaisonCard.vue'
+//
 </script>
 
 <template>
   <header>
     <h1 class="text-4xl">Test</h1>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Accueil</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/offres">Toutes les offres</RouterLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 
-  <main>
+  <main class="pt-8">
     <Suspense>
       <RouterView />
     </Suspense>
---
+++ src/pages/offres/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MaisonCard from '@/components/MaisonCard.vue'
+import { allMaisonsSorted } from '@/backend'
+
+const maisonsListe = await allMaisonsSorted()
+console.log(maisonsListe)
+</script>
+
+<template>
+  <div>
+    <h1 class="pb-8 text-xl">Toutes les offres</h1>
+    <MaisonCard v-for="uneMaison of maisonsListe" :v-key="uneMaison.id" v-bind="{ ...uneMaison }" />
+  </div>
+</template>
