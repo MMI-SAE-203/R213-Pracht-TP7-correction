cbdcd003a9f0fbd164e28b8676e6378087c44f51
pierre pracht <pierre.pracht@gmail.com>
2023-05-14 19:01:01 +0200

    offres en liste de lien

--- src/pages/offres/index.vue
+++ src/pages/offres/index.vue
@@ -9,6 +9,20 @@ console.log(maisonsListe)
 <template>
   <div>
     <h1 class="pb-8 text-xl">Toutes les offres</h1>
-    <MaisonCard v-for="uneMaison of maisonsListe" :v-key="uneMaison.id" v-bind="{ ...uneMaison }" />
+    <ul>
+      <li v-for="uneMaison of maisonsListe" :v-key="uneMaison.id" v-bind="{ ...uneMaison }">
+        <RouterLink
+          :to="{
+            name: 'offres-id',
+            params: {
+              id: uneMaison.id
+            }
+          }"
+          class="text-red-400 hover:text-red-600"
+        >
+          {{ uneMaison.nomMaison }}
+        </RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
